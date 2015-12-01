b36030c7df5ac9d72ffea94b2d6179b4bc238719
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2015-12-01 20:24:59 +0900

    :heavy_plus_sign: Add volume changer

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,9 +1,11 @@
+var volume = 50;
+
 function playSound(hour) {
 
   var audio = document.createElement('audio');
+  audio.volume = volume / 100;
   document.body.appendChild(audio);
   audio.autoplay = true;
 
-  var src = '../sounds/' + hour + '.mp3';
-  audio.src = src;
+  audio.src = '../sounds/' + hour + '.mp3';
 }
